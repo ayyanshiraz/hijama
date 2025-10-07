e5702464d4d180cec84a70a202359ebf16acdbe4
Ayan Shiraz <ayyanshiraz28@gmail.com>
2025-10-07 16:28:37 +0500

    feat: configure build settings for Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Add this line
+  /* You can add other config options here if you have them */
 };
 
 export default nextConfig;
