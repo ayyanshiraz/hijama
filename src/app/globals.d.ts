3ac7ca3756fd71ad05032f66e10ac464c785c819
YASIR IRSHAD <yasir.blackzero@gmail.com>
2025-12-01 15:59:23 +0500

    update

--- src/app/globals.d.ts
+++
@@ -1,9 +0,0 @@
-// globals.d.ts (YAHAN CHANGE KAREIN)
-
-interface Window {
-    // Return type boolean hona chahiye kyunki JavaScript function 'return false;' karta hai
-    gtag_report_conversion: (url: string) => boolean; 
-    gtagSendEvent: (url: string) => boolean; // <--- FIX 1: Type changed to boolean
-    
-    gtag: (...args: any[]) => void;
-}
